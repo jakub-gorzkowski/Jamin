16ec6b8063198f0b3a1acf801229f142b1d35668
Jakub <jakub.gorzkowski@student.pk.edu.pl>
2024-01-26 00:08:27 +0100

    Added layout for followed page with home.css

--- public/views/followed.php
+++ public/views/followed.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="public/style/global.css">
+        <link rel="stylesheet" href="public/style/followed.css">
         <script src="https://kit.fontawesome.com/2069fca16b.js" crossorigin="anonymous"></script>
         <title>Followed</title>
     </head>
@@ -19,5 +20,57 @@
                 <div class="nav-button"><a href="settings"><i class="fa-solid fa-gear"></i> Settings</a></div>
             </div>
         </nav>
+
+        <div class="section-container"> 
+            <div class="section-header-container">
+                <h2>Upcoming</h2>
+                <button class="show-all">Show all</button>
+            </div>
+            <div class="recommended-event">
+                <div class="image-container">
+                    <img src="public/uploads/example.jpg" alt="event-image">
+                </div>
+                <div class="information-container">
+                    <div class="event-name">
+                        <h3>Event name</h3>
+                    </div>
+                    <div class="description">
+                        <article>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</article>
+                    </div>
+                    <div class="details-container">
+                        <div class="location"><i class="fa-solid fa-location-dot"></i> Location</div>
+                        <div class="category"><i class="fa-solid fa-table-cells"></i> Category</div>
+                        <div class="price-range"><i class="fa-solid fa-money-bill-1-wave"></i> 20-60€</div>
+                        <button class="follow-button" type="button">Unfollow</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="section-container"> 
+            <div class="section-header-container">
+                <h2>Past events</h2>
+                <button class="show-all">Show all</button>
+            </div>
+            <div class="recommended-event">
+                <div class="image-container">
+                    <img src="public/uploads/example.jpg" alt="event-image">
+                </div>
+                <div class="information-container">
+                    <div class="event-name">
+                        <h3>Event name</h3>
+                    </div>
+                    <div class="description">
+                        <article>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</article>
+                    </div>
+                    <div class="details-container">
+                        <div class="location"><i class="fa-solid fa-location-dot"></i> Location</div>
+                        <div class="category"><i class="fa-solid fa-table-cells"></i> Category</div>
+                        <div class="price-range"><i class="fa-solid fa-money-bill-1-wave"></i> 20-60€</div>
+                        <button class="follow-button" type="button">Unfollow</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
