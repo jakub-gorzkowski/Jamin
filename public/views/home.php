4d74c6135231c630b5da05ec44e89783fe0e1811
Jakub <jakub.gorzkowski@student.pk.edu.pl>
2024-01-25 23:43:18 +0100

    Added proper layout for main sections with global.css and home page with home.css

--- public/views/home.php
+++ public/views/home.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="public/style/global.css">
+        <link rel="stylesheet" href="public/style/home.css">
         <script src="https://kit.fontawesome.com/2069fca16b.js" crossorigin="anonymous"></script>
         <title>Home</title>
     </head>
@@ -20,19 +21,43 @@
             </div>
         </nav>
 
-        <div id="promoted-events">
-            <h1>Promoted events</h1>
+        <div class="promoted-container">
+            <div class="section-header-container">
+                <h2>Promoted events</h2>
+            </div>
+
+            <div class="promoted-event">
+                <img src="public/uploads/testowe.jpeg" alt="promoted-event-image">
+                <div class="promoted-event-information">
+                    <h3>Event name</h3>
+                    <article>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</article>
+                </div>
+            </div>
         </div>
 
-        <div id="recommended-events"> 
-            <h1>Recommended for you</h1>
-            
-        </div>
-
-        <div id="content-container">
-            <img src="public/uploads/<?= $event->getImage() ?>">
-            <h2><?= $event->getTitle() ?></h2>
-            <p><?= $event->getDescription() ?></p>
+        <div class="section-container"> 
+            <div class="section-header-container">
+                <h2>Recommended for you</h2>
+            </div>
+            <div class="recommended-event">
+                <div class="image-container">
+                    <img src="public/uploads/example.jpg" alt="event-image">
+                </div>
+                <div class="information-container">
+                    <div class="event-name">
+                        <h3>Event name</h3>
+                    </div>
+                    <div class="description">
+                        <article>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</article>
+                    </div>
+                    <div class="details-container">
+                        <div class="location"><i class="fa-solid fa-location-dot"></i> Location</div>
+                        <div class="category"><i class="fa-solid fa-table-cells"></i> Category</div>
+                        <div class="price-range"><i class="fa-solid fa-money-bill-1-wave"></i> 20-60€</div>
+                        <button class="follow-button" type="button">Follow</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </body>
 </html>
